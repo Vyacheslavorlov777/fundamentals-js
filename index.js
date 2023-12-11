35a9885b2b7c0d1b54e26630d45f184f3eeb9e0c
Vyacheslav <Vyacheslav@MacBook-Air-M2.local>
2023-12-11 20:45:43 +0300

    3 out of 5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+export const concatStrings = (a , b) => a + b;
+
+export const isString = (value) => value === "";
+
+export function identifySign(num) {
+    if (num === 0) {
+        return "Ноль";
+    } if (num > 0) {
+        return "Положительное число";
+    } if (num < 0) {
+        return "Отрицательное число";
+    }
+}
+
+export function reverseWords(str) {
+    return str.split(' ').reverse().join(' ');
+}  
+
+export function wordsCount(text) {
+    return text.split(' ').length;
+}
+
 
